6d7d8012f53c4456258393b1494f5945809b15e9
Pier Dolique <perdgit.uieog@slmail.me>
2025-11-16 22:45:13 +0200

    feat(linting): add ESLint and oxlint
    - ✨ add eslint configuration file
    - ✨ add oxlint configuration file
    - �� update pre-commit hook with lint tasks
    - 🐛 fix App.vue formatting and simplify props
    - 🐛 add ESLint disable comment for false-positive warning in main.ts
    - 📦 eslint: v9.39.1
    - 📦 eslint-config-greenpie: v14.14.0
    - 📦 oxlint: v1.28.0

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './styles/style.css'
 import App from './App.vue'
 
+// FIXME: false-positive ESLint warning
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App).mount('#app')
